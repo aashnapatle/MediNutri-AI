b8ab0ca08dbed191aebde605c6622a3d1c7e9d07
aashnapatle <aashnapatle@gmail.com>
2026-05-10 00:25:42 +0530

    firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyf8KOi8BPk7EcotF2_YZL44pKmZcrWn0",
+  authDomain: "medinutri-ai.firebaseapp.com",
+  projectId: "medinutri-ai",
+  storageBucket: "medinutri-ai.firebasestorage.app",
+  messagingSenderId: "131617888133",
+  appId: "1:131617888133:web:b089a0e44d59e2dc4a16ec"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
